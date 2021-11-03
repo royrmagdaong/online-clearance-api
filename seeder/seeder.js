67a9a6d43f87d27ddfbd2dceb23838a80d6938d2
unknown <royrmagdaong@gmail.com>
2021-11-03 11:15:43 +0800

    refactor seeder

--- seeder/seeder.js
+++ seeder/seeder.js
@@ -49,109 +49,21 @@ seeder.connect(process.env.DATABASE_URL, { useUnifiedTopology: true, useNewUrlPa
                         'Clearance'
                     ], async ()=> {
 
-                        let user = await new User({
-                            role: roles.ADMIN,
-                            email: "admin@ptc.com",
-                            password: hashPassword,
-                            verificationCode: generateCode(),
-                            is_verified: true
-                        })
-                        await user.save(async(err, newUser)=>{
-                            if(err){ return console.log('failed to create user admin.') }
-                            else{
-                                console.log('User Admin created.')
-                            }
-                        })
-                        
+                        // create admin user
+                        await createAdminUser(roles, hashPassword)
+
                         // populate user and department
-                        for(let i = 0; i<department.length;i++){
-                            let user = await new User({
-                                role: roles.HEAD_DEPARTMENT,
-                                email: department[i].email,
-                                password: hashPassword,
-                                verificationCode: generateCode(),
-                                is_verified: true
-                            })
-                            user.save(async (err, newUser)=>{
-                                if(err){ return console.log('failed to create user department.') }
-                                if(newUser){
-                                    let dept = await new HeadDepartment({
-                                        user_id: newUser._id,
-                                        department_name: department[i].name,
-                                        in_charge: department[i].in_charge,
-                                        mobile_number: `0945875346${i}`,
-                                        telephone_number: `56533${i}`,
-                                        email: department[i].email
-                                    })
-                                    await dept.save(async (err, newDept) =>{
-                                        if(err){ return console.log('failed to create user department.') }
-                                        if(newDept){
-                                            console.log(newDept.department_name + ' department created.')
-                                        }else{
-                                            return console.log('failed to create user department.') 
-                                        }
-                                    })
-                                }else{
-                                    return console.log('failed to create user department.') 
-                                }
-                            })
-                        }
+                        await populateUserDepartment(roles, hashPassword)
                         
                         // populate user and student
-                        for(let i = 0; i<student.length;i++){
-                            let user = await new User({
-                                role: roles.STUDENT,
-                                email: student[i].email,
-                                password: hashPassword,
-                                verificationCode: generateCode(),
-                                is_verified: true
-                            })
-                            user.save(async (err, newUser)=>{
-                                if(err){ return console.log('failed to create user student.') }
-                                if(newUser){
-                                    let stdt = await new Student({
-                                        user_id: newUser._id,
-                                        email: student[i].email,
-                                        first_name: student[i].first_name,
-                                        last_name: student[i].last_name,
-                                        course: student[i].course,
-                                        section: student[i].section,
-                                        year_level: student[i].year_level
-                                    })
-                                    await stdt.save(async (err, newStudent) =>{
-                                        if(err){ return console.log('failed to create user student.') }
-                                        if(newStudent){
-                                            console.log(newStudent.email + ' student created.')
-                                        }else{
-                                            return console.log('failed to create user student.') 
-                                        }
-                                    })
-                                }else{
-                                    return console.log('failed to create user student.') 
-                                }
-                            })
-                        }
+                        await populateUserStudent(roles, hashPassword)
 
                         // populate course
-                        for(let i = 0; i<course.length;i++){
-                            let course_ = await new Course({
-                                code: course[i].code,
-                                description: course[i].description
-                            })
-                            await course_.save(async (err, newCourse) =>{
-                                if(err){ return console.log('failed to create course.') }
-                                if(newCourse){
-                                    console.log(newCourse.code + ' course created.')
-                                }else{
-                                    return console.log('failed to create course.') 
-                                }
-                            })
-                        }
+                        await populateCourse()
 
-                        setTimeout(()=>{
+                        await setTimeout(()=>{
                             seeder.disconnect();
-                        }, 2000)
-                        
+                        }, 1000)
                     })
                 }
             })
@@ -250,3 +162,107 @@ const course = [
         description: "Certificate in Hotel and Restaurant Management"
     }
 ]
+
+const populateCourse = async () => {
+    for(let i = 0; i<course.length;i++){
+        let course_ = await new Course({
+            code: course[i].code,
+            description: course[i].description
+        })
+        await course_.save((err, newCourse) =>{
+            if(err){ return console.log('failed to create course.') }
+            if(newCourse){
+                console.log(newCourse.code + ' course created.')
+            }else{
+                return console.log('failed to create course.') 
+            }
+        })
+    }
+}
+
+const populateUserStudent = async (roles, hashPassword) => {
+    for(let i = 0; i<student.length;i++){
+        let user = await new User({
+            role: roles.STUDENT,
+            email: student[i].email,
+            password: hashPassword,
+            verificationCode: generateCode(),
+            is_verified: true
+        })
+        user.save(async (err, newUser)=>{
+            if(err){ return console.log('failed to create user student.') }
+            if(newUser){
+                let stdt = await new Student({
+                    user_id: newUser._id,
+                    email: student[i].email,
+                    first_name: student[i].first_name,
+                    last_name: student[i].last_name,
+                    course: student[i].course,
+                    section: student[i].section,
+                    year_level: student[i].year_level
+                })
+                await stdt.save(async (err, newStudent) =>{
+                    if(err){ return console.log('failed to create user student.') }
+                    if(newStudent){
+                        console.log(newStudent.email + ' student created.')
+                    }else{
+                        return console.log('failed to create user student.') 
+                    }
+                })
+            }else{
+                return console.log('failed to create user student.') 
+            }
+        })
+    }
+}
+
+const populateUserDepartment = async (roles, hashPassword) => {
+    for(let i = 0; i<department.length;i++){
+        let user = await new User({
+            role: roles.HEAD_DEPARTMENT,
+            email: department[i].email,
+            password: hashPassword,
+            verificationCode: generateCode(),
+            is_verified: true
+        })
+        user.save(async (err, newUser)=>{
+            if(err){ return console.log('failed to create user department.') }
+            if(newUser){
+                let dept = await new HeadDepartment({
+                    user_id: newUser._id,
+                    department_name: department[i].name,
+                    in_charge: department[i].in_charge,
+                    mobile_number: `0945875346${i}`,
+                    telephone_number: `56533${i}`,
+                    email: department[i].email
+                })
+                await dept.save(async (err, newDept) =>{
+                    if(err){ return console.log('failed to create user department.') }
+                    if(newDept){
+                        console.log(newDept.department_name + ' department created.')
+                    }else{
+                        return console.log('failed to create user department.') 
+                    }
+                })
+            }else{
+                return console.log('failed to create user department.') 
+            }
+        })
+    }
+}
+
+const createAdminUser = async (roles, hashPassword) => {
+    let user = await new User({
+        role: roles.ADMIN,
+        email: "admin@ptc.com",
+        password: hashPassword,
+        verificationCode: generateCode(),
+        is_verified: true
+    })
+    await user.save(async(err, newUser)=>{
+        if(err){ return console.log('failed to create user admin.') }
+        else{
+            console.log('User Admin created.')
+        }
+    })
+}
