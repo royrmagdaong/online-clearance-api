9b53c8095460bbb0533ce5738c498d0e1182239d
unknown <royrmagdaong@gmail.com>
2021-12-05 22:52:09 +0800

    add update school year in SchoolYearController

--- controllers/SchoolYearController.js
+++ controllers/SchoolYearController.js
@@ -8,7 +8,30 @@ module.exports = {
                 if(schoolyear){
                     return res.status(200).json({response:true,data:schoolyear})
                 }else{
-                    return res.status(200).json({response:false, message:'nothing found'})
+                    return res.json({response:false, message:'nothing found'})
+                }
+            })
+        } catch (error) {
+            return res.status(500).json({response:false, message:error.message})
+        }
+    },
+    updateSchoolYear: async (req, res) => {
+        try {
+            let semester = req.body.semester
+            let academic_year = req.body.academic_year
+
+            await SchoolYear.findOne({}).exec(async (error,schoolyear)=>{
+                if(error) res.status(500).json({response:false, message:error.message})
+                if(schoolyear){
+                    schoolyear.SEMESTER = semester
+                    schoolyear.ACADEMIC_YEAR = academic_year
+                    schoolyear.updated_at = Date.now()
+                    schoolyear.save(async error=>{
+                        if(error) return res.status(500).json({response:false, message:error.message})
+                        return res.status(200).json({response:true, message: 'School year updated successfully!'})
+                    })
+                }else{
+                    return res.json({response:false, message:'nothing found'})
                 }
             })
         } catch (error) {
--- routes/SchoolYear.js
+++ routes/SchoolYear.js
@@ -4,11 +4,17 @@ const SchoolYearController = require('../controllers/SchoolYearController')
 const authenticate = require('../middlewares/authenticate')
 const authRole = require('../middlewares/authRole')
 
-// get all users
+// get school year
 router.post('/', 
     authenticate, 
     authRole(['admin','head-department','student']), 
     SchoolYearController.getSchoolYear
+)
+// update school year
+router.post('/update', 
+    authenticate, 
+    authRole(['admin']), 
+    SchoolYearController.updateSchoolYear
 )
 
 module.exports = router
